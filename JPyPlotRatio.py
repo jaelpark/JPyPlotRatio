6ba9a9b53312baed17485432d4c43ce1dabf9a34
jparkkil <jasper.parkkila@cern.ch>
2020-04-15 17:24:04 +0300

    General updates

--- JPyPlotRatio.py
+++ JPyPlotRatio.py
@@ -27,7 +27,7 @@ def TGraphErrorsToNumpy(gr):
 	return x,y,xerr,yerr;
 
 class JPyPlotRatio:
-	def __init__(self, panels=(1,1), panelsize=(3,3.375), rowBounds={}, ratioBounds = {}, panelScaling={}, panelLabel = {}, **kwargs):
+	def __init__(self, panels=(1,1), panelsize=(3,3.375), rowBounds={}, ratioBounds = {}, panelScaling={}, panelLabel = {}, panelLabelLoc=(0.2,0.92), panelLabelSize=16, panelLabelAlign="right", legendLoc=(0.52,0.28), legendSize=10, **kwargs):
 		self.p,self.ax = plt.subplots(2*panels[0],panels[1]+1,sharex=True,figsize=(panels[1]*panelsize[0],panels[0]*panelsize[1]),gridspec_kw={'width_ratios':[0.0]+panels[1]*[1.0],'height_ratios':panels[0]*[0.7,0.3]});
 		self.p.subplots_adjust(wspace=0.0,hspace=0.0);
 
@@ -37,8 +37,13 @@ class JPyPlotRatio:
 
 		self.panelScaling = panelScaling;
 		self.panelLabel = panelLabel;
+		self.panelLabelLoc = panelLabelLoc;
+		self.panelLabelSize = panelLabelSize;
+		self.panelLabelAlign = panelLabelAlign;
 		self.rowBounds = rowBounds;
 		self.ratioBounds = ratioBounds;
+		self.legendLoc = legendLoc;
+		self.legendSize = legendSize;
 
 		try:
 			self.ax.flat[0].set_xlim(kwargs['xlim']);
@@ -62,22 +67,22 @@ class JPyPlotRatio:
 		for A in self.ax.flat:
 			A.tick_params(which="major",direction="in",length=8.0);
 			A.tick_params(which="minor",direction="in",length=2.8);
-			#A.xaxis.set_major_locator(plticker.MultipleLocator(10.0));
-			#A.xaxis.set_minor_locator(plticker.AutoMinorLocator(5));
+			A.xaxis.set_major_locator(plticker.MultipleLocator(1.0));
+			A.xaxis.set_minor_locator(plticker.AutoMinorLocator(0.5));
 			#A.yaxis.set_minor_locator(plticker.AutoMinorLocator(5));
 			A.xaxis.set_tick_params(labelsize=13);
 			A.yaxis.set_tick_params(labelsize=13);
 	
-	def Add(self, plotIndex, arrays, label="", **kwargs):
-		self.plots.append((plotIndex,arrays,label,kwargs));
+	def Add(self, plotIndex, arrays, label="", plotType="data", **kwargs):
+		self.plots.append((plotIndex,arrays,label,plotType,kwargs));
 		self.usedSet.add(plotIndex);
 
 		return len(self.plots)-1; #handle to the plot, given to the Ratio()
 	
-	def AddTGraph(self, plotIndex, gr, label="", **kwargs):
+	def AddTGraph(self, plotIndex, gr, label="", plotType="data", **kwargs):
 		#arrays = TGraphErrorsToNumpy(gr);
 		x,y,_,yerr = TGraphErrorsToNumpy(gr);
-		return self.Add(plotIndex,(x,y,yerr),label,**kwargs);
+		return self.Add(plotIndex,(x,y,yerr),label,plotType,**kwargs);
 	
 	def Ratio(self, r1, r2):
 		self.ratios.append((r1,r2));
@@ -96,7 +101,12 @@ class JPyPlotRatio:
 
 		#plot the data
 		for plot in self.plots:
-			labels[plot[2]] = self.ax.flat[a0[plot[0]]].errorbar(*plot[1],**plot[3]);
+			if plot[3] == "data":
+				labels[plot[2]] = self.ax.flat[a0[plot[0]]].errorbar(*plot[1],**plot[4]);
+			elif plot[3] == "theory":
+				p1 = self.ax.flat[a0[plot[0]]].fill_between(plot[1][0],plot[1][1]-plot[1][2],plot[1][1]+plot[1][2],**plot[4]);
+				labels[plot[2]] = (p1,
+					self.ax.flat[a0[plot[0]]].plot(*plot[1][0:2],color=p1.get_edgecolor()[0],linestyle=p1.get_linestyle()[0])[0]);
 			
 		for i,ra0n in enumerate(A0[:,]):
 			try:
@@ -132,9 +142,17 @@ class JPyPlotRatio:
 			ratio = y1d/y2d;
 			ratio_err = ratio*np.sqrt((yerr2d/y2d)**2+(yerr1d/y1d)**2);
 
+			m = ~np.isnan(ratio);
+			sx = sx[m];
+			ratio = ratio[m];
+			ratio_err = ratio_err[m];
+
 			plotIndex = self.plots[robj[0]][0];
-			self.ax.flat[a1[plotIndex]].fill_between(sx,ratio-ratio_err,ratio+ratio_err,color=self.plots[robj[1]][3]["color"],alpha=0.5);
-			self.ax.flat[a1[plotIndex]].plot(sx,ratio,linestyle="-",color=self.plots[robj[1]][3]["color"]);
+			if self.plots[robj[1]][3] == "data":
+				self.ax.flat[a1[plotIndex]].errorbar(sx,ratio,ratio_err,**self.plots[robj[1]][4]);
+			elif self.plots[robj[1]][3] == "theory":
+				p1 = self.ax.flat[a1[plotIndex]].fill_between(sx,ratio-ratio_err,ratio+ratio_err,**self.plots[robj[1]][4]);
+				self.ax.flat[a1[plotIndex]].plot(sx,ratio,color=p1.get_edgecolor()[0],linestyle=p1.get_linestyle()[0]);
 
 		#adjust ticks
 		for ra0,ra1,rap in zip(a0,a1,ap):
@@ -151,7 +169,7 @@ class JPyPlotRatio:
 				self.ax.flat[ra0].set_ylim(ylim1);
 
 			try:
-				self.ax.flat[ra0].text(0.2,0.92,self.panelLabel[rap],horizontalalignment="right",verticalalignment="center",transform=self.ax.flat[ra0].transAxes,size=16);
+				self.ax.flat[ra0].text(*self.panelLabelLoc,self.panelLabel[rap],horizontalalignment=self.panelLabelAlign,verticalalignment="center",transform=self.ax.flat[ra0].transAxes,size=self.panelLabelSize);
 			except KeyError:
 				pass;
 
@@ -182,7 +200,7 @@ class JPyPlotRatio:
 
 		lines = [labels[p] for p in labels];
 		lines = [h[0] if isinstance(h,container.ErrorbarContainer) else h for h in lines];
-		self.ax[0,1].legend(lines,labels,frameon=False,prop={'size':10},loc="center",handletextpad=0.1,bbox_to_anchor=(0.52,0.28));
+		self.ax[0,1].legend(lines,labels,frameon=False,prop={'size':self.legendSize},loc="center",handletextpad=0.1,bbox_to_anchor=self.legendLoc);
 	
 	def Save(self, filename):
 		self.p.savefig(filename,bbox_inches="tight");
